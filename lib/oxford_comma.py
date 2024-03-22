801f086b15d459e36019e492fd78f30714aaeb30
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-22 11:03:43 -0700

    complete oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ''.join(items)
+    elif len(items) ==2:
+        return items[0] + " and " + items[1]
+    else:
+        length = len(items) - 1
+        all_but_last = items[0:length]
+        first_words = ", ".join(all_but_last)
+        return first_words + ", and " + items[length]
+
